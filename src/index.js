05b045559d30e606691ab191002ea0ae04e57832
PavloNa <pavlo.escola@gmail.com>
2025-02-04 15:58:29 +0000

    Added login template and no page found template

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NoPage from './components/NoPage/NoPage';
+import Login from './components/Login/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<App />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="*" element={<NoPage />} />
+    </Routes>
+  </BrowserRouter>
 );
 
