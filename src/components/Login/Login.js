a00c8431ec9f27c5163274acba8716bddf7cf27f
PavloNa <pavlo.escola@gmail.com>
2025-02-04 16:25:10 +0000

    Added icons library

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='wrapper'>
+        <form action=''>
+            <h1>Login</h1>
+            <div className='input-box'>
+                <input type='text' placeholder='Username' required />
+            </div>
+            <div className="input-box">
+                <input type="text" placeholder='Password' required/>
+            </div>
+            <div className="remember-forgot">
+                <label><input type="checkbox" />Remember Me</label>
+                <a href="#">Forgot password?</a>
+            </div>
+            <button type='submit'>Login</button>
+            <div className="register-link">
+                <p>Don't have an account? <a href="#">Register</a></p>
+            </div>
+        </form>
+    </div>
   )
 }
 
